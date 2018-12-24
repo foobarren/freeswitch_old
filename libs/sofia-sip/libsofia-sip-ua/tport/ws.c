karlma/freeswitch-b1.10

FS-10150: [freeswitch-core] Reduce writes to closed ssl sockets -- same fix for non-ssl sockets #resolve

libs/sofia-sip/libsofia-sip-ua/tport/ws.c
@@ -476,7 +476,13 @@ ssize_t ws_raw_write(wsh_t *wsh, void *data, size_t bytes)
 			ms_sleep(ms);
 		}
 
-	} while (--sanity > 0 && ((r == -1 && xp_is_blocking(xp_errno())) || (wsh->block && wrote < bytes)));
+		if (r == -1) {
+			if (!ks_errno_is_blocking(ks_errno())) {
+				break;
+			}
+		}
+
+	} while (--sanity > 0 && kws->block && wrote < bytes);
 
 	//if (r<0) {
 		//printf("wRITE FAIL: %s\n", strerror(errno));

src/mod/endpoints/mod_verto/ws.c
@@ -476,7 +476,13 @@ ssize_t ws_raw_write(wsh_t *wsh, void *data, size_t bytes)
 			ms_sleep(ms);
 		}
 
-	} while (--sanity > 0 && ((r == -1 && xp_is_blocking(xp_errno())) || (wsh->block && wrote < bytes)));
+		if (r == -1) {
+			if (!ks_errno_is_blocking(ks_errno())) {
+				break;
+			}
+		}
+
+	} while (--sanity > 0 && kws->block && wrote < bytes);
 
 	//if (r<0) {
 		//printf("wRITE FAIL: %s\n", strerror(errno));

foobarren/freeswitch_old

libs/sofia-sip/libsofia-sip-ua/tport/ws.c
@@ -455,7 +455,13 @@ ssize_t ws_raw_write(wsh_t *wsh, void *data, size_t bytes)
 			ms_sleep(1);
 		}
 
-	} while (--sanity > 0 && ((r == -1 && xp_is_blocking(xp_errno())) || (wsh->block && wrote < bytes)));
+		if (r == -1) {
+			if (!xp_is_blocking(xp_errno())) {
+				break;
+			}
+		}
+
+	} while (--sanity > 0 && wsh->block && wrote < bytes);
 
 	//if (r<0) {
 		//printf("wRITE FAIL: %s\n", strerror(errno));

src/mod/endpoints/mod_verto/ws.c
@@ -454,9 +454,15 @@ ssize_t ws_raw_write(wsh_t *wsh, void *data, size_t bytes)
 		if (sanity < 2000) {
 			ms_sleep(1);
 		}
-		
-	} while (--sanity > 0 && ((r == -1 && xp_is_blocking(xp_errno())) || (wsh->block && wrote < bytes)));
-	
+
+		if (r == -1) {
+			if (!xp_is_blocking(xp_errno())) {
+				break;
+			}
+		}
+
+	} while (--sanity > 0 && wsh->block && wrote < bytes);
+
 	//if (r<0) {
 		//printf("wRITE FAIL: %s\n", strerror(errno));
 	//}